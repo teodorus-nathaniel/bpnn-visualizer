ec2a3b7fab155192fefc5d3334bdf7b459f5bacf
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-03-07 07:23:51 +0700

    added event listener for start btn

--- src/core/network-model-controller.js
+++ src/core/network-model-controller.js
@@ -1,50 +1,56 @@
 import generateModel from '../utilities/generate-model';
+import startBPNN from './bpnn-animation';
 
 const layerInputContainer = document.getElementById('layer-input-container');
 const layerCountInput = document.getElementById('layer-count-input');
+const startButton = document.getElementById('start-btn');
 
 let layers = [ 3, 2, 10, 2 ];
 
-export default function initModelController (stage){
-  let network;
-  const resetModel = () => {
-    stage.removeChildren();
-    network = generateModel(stage, layers);
-  };
+export default function initModelController(stage) {
+	let network;
+	const resetModel = () => {
+		stage.removeChildren();
+		network = generateModel(stage, layers);
+	};
 
-  function updateLayer (value, idx){
-    if (layers[idx] === value) return;
-    layers[idx] = value;
-    resetModel();
-  }
+	function updateLayer(value, idx) {
+		if (layers[idx] === value) return;
+		layers[idx] = value;
+		resetModel();
+	}
 
-  function initNeuronInputListener (){
-    Array.from(layerInputContainer.children).forEach((input, idx) => {
-      input.addEventListener('change', function (){
-        updateLayer(+this.value, idx);
-      });
-    });
-  }
+	function initNeuronInputListener() {
+		Array.from(layerInputContainer.children).forEach((input, idx) => {
+			input.addEventListener('change', function() {
+				updateLayer(+this.value, idx);
+			});
+		});
+	}
 
-  resetModel();
-  initNeuronInputListener();
+	resetModel();
+	initNeuronInputListener();
 
-  layerCountInput.addEventListener('change', function (){
-    const layerCount = +this.value;
-    if (layers.length < layerCount) {
-      for (let i = 0; i < layerCount - layers.length; i++) layers.push(3);
-    } else if (layers.length > layerCount) {
-      layers = layers.slice(0, layerCount);
-    } else return;
+	layerCountInput.addEventListener('change', function() {
+		const layerCount = +this.value;
+		if (layers.length < layerCount) {
+			for (let i = 0; i < layerCount - layers.length; i++) layers.push(3);
+		} else if (layers.length > layerCount) {
+			layers = layers.slice(0, layerCount);
+		} else return;
 
-    layerInputContainer.innerHTML = Array.from({ length: layerCount }).reduce(
-      (acc, _, idx) =>
-        acc +
-        `<input type="text" placeholder="neuron for layer ${idx + 1}...">`,
-      ''
-    );
+		layerInputContainer.innerHTML = Array.from({ length: layerCount }).reduce(
+			(acc, _, idx) =>
+				acc +
+				`<input type="text" placeholder="neuron for layer ${idx + 1}...">`,
+			''
+		);
 
-    resetModel();
-    initNeuronInputListener();
-  });
+		resetModel();
+		initNeuronInputListener();
+	});
+
+	startButton.addEventListener('click', () =>
+		startBPNN(network.neurons, network.weights)
+	);
 }
