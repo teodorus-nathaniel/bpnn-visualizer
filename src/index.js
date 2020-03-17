856afe2ec908965d69d29ed13751567d6311c3d6
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-03-17 17:00:08 +0700

    added open close modal

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 import * as PIXI from 'pixi.js';
-import generateModel from './utilities/generate-model';
-import { forEachElement } from './utilities/array-utilities';
 import initForm from './utilities/form-control';
 import PixiPlugin from 'gsap/PixiPlugin';
 import gsap from 'gsap';
-import startBPNN from './core/bpnn-animation';
 import initModelController from './core/network-model-controller';
-import bpnn from './core/bpnn-calculation';
 import initInputDataListeners from './utilities/input-data-control';
+import initModalControl from './utilities/input-modal-control';
 
-PixiPlugin.registerPIXI(PIXI);
-gsap.registerPlugin(PixiPlugin);
+document.addEventListener('DOMContentLoaded', () => {
+	PixiPlugin.registerPIXI(PIXI);
+	gsap.registerPlugin(PixiPlugin);
 
-const app = new PIXI.Application({
-	view: document.getElementById('pixi-canvas'),
-	backgroundColor: 0x000000,
-	height: window.innerHeight,
-	width: window.innerWidth,
-	antialias: true
+	const app = new PIXI.Application({
+		view: document.getElementById('pixi-canvas'),
+		backgroundColor: 0x000000,
+		height: window.innerHeight,
+		width: window.innerWidth,
+		antialias: true
+	});
+
+	app.stage.sortableChildren = true;
+
+	initModelController(app.stage);
+	initInputDataListeners();
+	initModalControl();
+
+	// const layers = [ 2, 4, 2, 10, 2 ];
+
+	// const { neurons, weights } = generateModel(app.stage, layers);
+	// startBPNN(neurons, weights, 1);
+
+	initForm('csv-form', 'file-input');
+
+	// forEachElement(neurons, (element) => app.stage.addChild(element));
+	// forEachElement(weights, (element) => app.stage.addChild(element));
 });
-
-app.stage.sortableChildren = true;
-
-initModelController(app.stage);
-initInputDataListeners();
-
-// const layers = [ 2, 4, 2, 10, 2 ];
-
-// const { neurons, weights } = generateModel(app.stage, layers);
-// startBPNN(neurons, weights, 1);
-
-initForm('csv-form', 'file-input');
-
-// forEachElement(neurons, (element) => app.stage.addChild(element));
-// forEachElement(weights, (element) => app.stage.addChild(element));
