98365087a4be0ee6a5f64de24d118e8bc1be8231
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-03-18 20:58:05 +0700

    neumorphism design

--- src/controllers/input-data-control.js
+++ src/controllers/input-data-control.js
@@ -6,8 +6,14 @@ const dataContainer = document.getElementById('data');
 const titleContainer = document.getElementById('features-titles');
 const addNewFeatureBtn = document.getElementById('add-new-feature');
 const addNewDataBtn = document.getElementById('add-new-data');
+const modal = document.getElementById('custom-input-modal');
 
-export default function initInputDataListeners (){
+const inputDimensionRowText = document.getElementById('input-dimension--row');
+const inputDimensionColumnText = document.getElementById(
+  'input-dimension--column'
+);
+
+function initDeleteDataset (){
   dataContainer.addEventListener('click', (e) => {
     const closest = e.target.closest('.delete-row-btn');
     if (!closest) return;
@@ -15,7 +21,9 @@ export default function initInputDataListeners (){
     const parent = closest.closest('div');
     dataContainer.removeChild(parent);
   });
+}
 
+function initDeleteFeature (){
   titleContainer.addEventListener('click', (e) => {
     const closest = e.target.closest('.delete-icon');
     if (!closest || titleContainer.children.length <= 3) return;
@@ -30,7 +38,9 @@ export default function initInputDataListeners (){
       row.removeChild(row.children[index]);
     });
   });
+}
 
+function initAddFeature (){
   addNewFeatureBtn.addEventListener('click', () => {
     const titles = titleContainer.children;
     const datas = dataContainer.children;
@@ -55,7 +65,9 @@ export default function initInputDataListeners (){
       addNewFeatureBtn.classList.remove('hide');
     }
   });
+}
 
+function initAddDataset (){
   addNewDataBtn.addEventListener('click', () => {
     const featuresLength = titleContainer.children.length - 1;
     dataContainer.insertAdjacentHTML(
@@ -63,4 +75,26 @@ export default function initInputDataListeners (){
       getInputRowDataComponent(featuresLength)
     );
   });
+}
+
+function initOpenCloseCustomInputModal (){
+  const closeModalBtn = document.getElementById('close-modal-btn');
+  closeModalBtn.addEventListener('click', () => {
+    modal.classList.add('hide');
+    inputDimensionColumnText.textContent = titleContainer.children.length - 2;
+    inputDimensionRowText.textContent = dataContainer.children.length;
+  });
+  const openModalBtn = document.getElementsByClassName('open-modal-btn');
+  console.log(openModalBtn);
+  Array.from(openModalBtn).forEach((el) =>
+    el.addEventListener('click', () => modal.classList.remove('hide'))
+  );
+}
+
+export default function initInputDataListeners (){
+  initAddDataset();
+  initDeleteDataset();
+  initAddFeature();
+  initDeleteFeature();
+  initOpenCloseCustomInputModal();
 }
--- src/index.js
+++ src/index.js
@@ -3,35 +3,33 @@ import PixiPlugin from 'gsap/PixiPlugin';
 import gsap from 'gsap';
 import initModelController from './controllers/network-model-control';
 import initInputDataListeners from './controllers/input-data-control';
-import initModalControl from './controllers/input-modal-control';
 import initInputTypeChangeListener from './controllers/input-type-control';
 
 document.addEventListener('DOMContentLoaded', () => {
-	PixiPlugin.registerPIXI(PIXI);
-	gsap.registerPlugin(PixiPlugin);
+  PixiPlugin.registerPIXI(PIXI);
+  gsap.registerPlugin(PixiPlugin);
 
-	const app = new PIXI.Application({
-		view: document.getElementById('pixi-canvas'),
-		backgroundColor: 0x000000,
-		height: window.innerHeight,
-		width: window.innerWidth,
-		antialias: true
-	});
+  const app = new PIXI.Application({
+    view: document.getElementById('pixi-canvas'),
+    backgroundColor: 0x000000,
+    height: window.innerHeight,
+    width: window.innerWidth,
+    antialias: true
+  });
 
-	app.stage.sortableChildren = true;
+  app.stage.sortableChildren = true;
 
-	initModelController(app.stage);
-	initInputDataListeners();
-	initModalControl();
-	initInputTypeChangeListener();
+  initModelController(app.stage);
+  initInputDataListeners();
+  initInputTypeChangeListener();
 
-	// const layers = [ 2, 4, 2, 10, 2 ];
+  // const layers = [ 2, 4, 2, 10, 2 ];
 
-	// const { neurons, weights } = generateModel(app.stage, layers);
-	// startBPNN(neurons, weights, 1);
+  // const { neurons, weights } = generateModel(app.stage, layers);
+  // startBPNN(neurons, weights, 1);
 
-	// initForm('csv-form', 'file-input');
+  // initForm('csv-form', 'file-input');
 
-	// forEachElement(neurons, (element) => app.stage.addChild(element));
-	// forEachElement(weights, (element) => app.stage.addChild(element));
+  // forEachElement(neurons, (element) => app.stage.addChild(element));
+  // forEachElement(weights, (element) => app.stage.addChild(element));
 });
